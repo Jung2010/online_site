d97862352c3b3d3a88b2aad5933a7f01eea3214c
HJ25 <70260230+Jung2010@users.noreply.github.com>
2024-07-03 01:04:00 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<html>
+  <head>
+    <?php include('lib/basicHead.php'); ?>
+  </head>
+  <body>
+    <h1 class='title'>Title</h1>
+    Input Test :
+    <input class='ipt' value='test'>
+    <select class='ipt'>
+      <option>HTML</option>
+      <option>CSS</option>
+      <option>Javascript</option>
+    </select>
+    <input class='ipt' type='date' value='2024-07-02'>
+    <button class='btn'>Button</button> <br>
+    <ol class='numberList'>
+      <li><b>HTML</b> : HyperText Markup Language</li>
+      <li><b>CSS</b> : Cascading Style Sheets</li>
+      <li><b>Javascript</b> : Interpreter Language</li>
+    </ol>
+    <ul class='dotList'>
+      <li><b>HTML</b> : HyperText Markup Language</li>
+      <li><b>CSS</b> : Cascading Style Sheets</li>
+      <li><b>Javascript</b> : Interpreter Language</li>
+    </ul>
+    <details class='detail'>
+      <summary>Lorem ipsum</summary>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere volutpat ex ac aliquam. Maecenas varius ullamcorper velit. Nullam eget ex nec turpis tempor commodo. Praesent sed lacinia justo. Proin finibus vitae felis nec vulputate. In hac habitasse platea dictumst. Vestibulum sodales interdum sem, ac laoreet neque laoreet a. Vestibulum vehicula erat dapibus justo lacinia, pulvinar tincidunt erat dignissim. Quisque sit amet nisi id magna tristique accumsan quis ac odio. Nullam vehicula egestas euismod. Ut in felis vel metus finibus commodo. Fusce consectetur condimentum erat eu sollicitudin.
+    </details>
+    <fieldset class='fieldset'>
+      <legend>HTML</legend>
+      <div>
+        <p>
+          <ul class='dotList'>
+            <li>HTML stands for Hyper Text Markup Language</li>
+            <li>HTML is the standard markup language for creating Web pages</li>
+            <li>HTML describes the structure of a Web page</li>
+            <li>HTML consists of a series of elements</li>
+            <li>HTML elements tell the browser how to display the content</li>
+            <li>HTML elements label pieces of content such as "this is a heading", "this is a paragraph", "this is a link", etc.</li>
+          </ul>
+        </p>
+        <p>
+          <a class='link' href='https://www.w3schools.com/html/html_intro.asp'>View more Informations ></a>
+        </p>
+      </div>
+    </fieldset>
+  </body>
+</html>
---
+++ lib/basicHead.php
@@ -0,0 +1,4 @@
+<?php include('lib/dats.php'); ?>
+<link rel="stylesheet" href="styles/basic.css">
+<?php include('lib/metas.php'); ?>
+<title><?=$siteName?></title>
